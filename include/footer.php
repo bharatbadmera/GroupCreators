648dca29fb7ec7aed9f94022385a76e7b761c5a8
bharatbadmera <bharatbadmera@gmail.com>
2019-04-04 00:02:41 +0530

    adding files

---
+++ include/footer.php
@@ -0,0 +1,3 @@
+	<div class="footer-wrap bg-white pd-20 mb-20 border-radius-5 box-shadow">
+		Group Creators - A solution to manage projects <a href="#" target="_blank"></a>
+	</div>
---
+++ include/sidebar.php
@@ -0,0 +1,19 @@
+	<div class="left-side-bar">
+		<div class="brand-logo">
+			<a href="index.php">
+				<img src="vendors/images/temp2.png" alt="">
+			</a>
+		</div>
+		<div class="menu-block customscroll">
+			<div class="sidebar-menu">
+					<ul id="accordion-menu">
+                        <li clas="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="fa fa-home"></span><span class="mtext">Home</span>
+						</a>
+					</li>
+                </ul>
+
+			</div>
+		</div>
+	</div>
